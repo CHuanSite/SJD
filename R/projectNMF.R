e09686addbdc99059bfb1e32201d9ef159514394
Guangyan Li <601449089@qq.com>
2023-10-05 20:59:38 -0400

    added error handling, gene matching,1 proj_dataset

--- R/projectNMF.R
+++ R/projectNMF.R
@@ -16,45 +16,51 @@
 #' @keywords projection, joint, NMF
 #'
 #' @examples
-#' Example 1 (1 matrix in proj_dataset):
 #' proj_dataset = list(matrix(runif(5000, 1, 2), nrow = 100, ncol = 50))
 #' proj_group = list(c(TRUE, TRUE)) # which groupings in the joint decomposition you want to project on.
-#' list_component = jointNMF$linked_component_list
+#' list_component = jointNMF$linked_component_list # from jointNMF result
 #' res_projNMF = projectNMF(
 #' proj_dataset = proj_dataset,
 #' proj_group = proj_group,
 #' list_component = list_component)
 #' 
-#' Example 2 (2 matrix in proj_dataset):
-#' proj_dataset = list(matrix(runif(5000, 1, 2), nrow = 100, ncol = 50), 
-#'                     matrix(runif(5000, 1, 2), nrow = 100, ncol = 50))
-#' proj_group = list(c(TRUE, TRUE), c(TRUE, TRUE))
-#' list_component = jointNMF$linked_component_list
-#' res_projNMF = projectNMF(
-#' proj_dataset = proj_dataset,
-#' proj_group = proj_group,
-#' list_component = list_component)
+#' PLEASE MAKE SURE YOUR proj_dataset AND list_component ELEMENTS HAVE MEANINGFUL ROW(GENE) NAMES. 
 
 
 
 ## Projection function
+
 projectNMF <- function(proj_dataset, proj_group, list_component, weighting=NULL, max_ite = 1000, max_err = 0.0001, enable_normalization = TRUE, column_sum_normalization = FALSE){
-  p = nrow(proj_dataset[[1]])
-  # Initialize the matrix W with zeros
-  W <- matrix(0, nrow = p, ncol = sum(comp_num))
-  K = length(proj_group[[1]])
-  # Reconstruct matrix W using list_component and comp_num
-  start_col <- 1
-  for (i in 1:K) {
-    end_col <- start_col + comp_num[i] - 1
-    W[, start_col:end_col] <- list_component[[i]]
-    start_col <- end_col + 1
-  }
-  comp_num = unlist(lapply(list_component, function(x) if (length(x) > 0) ncol(x)))
-  
-  
-  proj_list_score = list()
   if(!is.null(proj_dataset)){
+    if (length(proj_group[[1]]) != length(list_component)){
+      stop("Error:length of each element in proj_group should equal to length of list_component.")
+    }
+    common_genes = match(rownames(proj_dataset[[1]]), rownames(list_component[[1]]))
+    orig_gene_length = nrow(proj_dataset[[1]])
+    proj_dataset[[1]] = proj_dataset[[1]][common_genes,]
+    for (i in 1:length(list_component)) {
+      list_component[[i]] = list_component[[i]][common_genes,]
+    }
+    print(paste("Input", orig_gene_length, "genes in proj_dataset, found", nrow(proj_dataset[[1]]), "genes in common."))
+    
+    p = nrow(proj_dataset[[1]])
+    comp_num = unlist(lapply(list_component, function(x) if (length(x) > 0) ncol(x)))
+    
+    # Initialize the matrix W with zeros
+    W <- matrix(0, nrow = p, ncol = sum(comp_num))
+    K = length(proj_group[[1]])
+    # Reconstruct matrix W using list_component and comp_num
+    start_col <- 1
+    for (i in 1:K) {
+      end_col <- start_col + comp_num[i] - 1
+      W[, start_col:end_col] <- list_component[[i]]
+      start_col <- end_col + 1
+    }
+    
+    
+    
+    proj_list_score = list()
+
     proj_sample_name = sampleNameExtractor(proj_dataset)
     proj_dataset_name = datasetNameExtractor(proj_dataset)
     group_name = paste0("group", 1:length(proj_group[[1]])) # group name alternative 
@@ -66,31 +72,24 @@ projectNMF <- function(proj_dataset, proj_group, list_component, weighting=NULL,
     N = length(proj_dataset)
     M = sum(comp_num) 
     p = nrow(proj_dataset[[1]])
-    N_dataset = unlist(lapply(proj_dataset, ncol))
+    N_dataset = ncol(proj_dataset[[1]])
     
     
     ## Initialize the W and H for Nonnegative Matrix Factorization
     max_element = -Inf
     min_element = Inf
-    for(i in 1 : N){
-      max_element = max(max_element, max(proj_dataset[[i]]))
-      min_element = min(min_element, min(proj_dataset[[i]]))
-    }
+    max_element = max(max_element, max(proj_dataset[[1]]))
+    min_element = min(min_element, min(proj_dataset[[1]]))
     
     
     ## Initialize the values of W and H
-    X = c()
-    for(i in 1 : N){
-      X = cbind(X, proj_dataset[[i]])
-    }
+    X = proj_dataset[[1]]
+
+    
     H = c()
-    
-    
     for(i in 1 : K){ # K= length(group) = 3 in the example
       H_temp = c()
-      for(j in 1 : N){ # K = length(proj_dataset) = 1 in the example
-        H_temp = cbind(H_temp, matrix(runif(N_dataset[j] * comp_num[i], min_element, max_element), nrow = comp_num[i], ncol = N_dataset[j]))
-      }
+      H_temp = matrix(runif(N_dataset * comp_num[i], min_element, max_element), nrow = comp_num[i], ncol = N_dataset)
       H = rbind(H, H_temp)
     }
     
@@ -113,24 +112,27 @@ projectNMF <- function(proj_dataset, proj_group, list_component, weighting=NULL,
       }
     }
     
-    
-    for(i in 1 : N){  # i goes to 1 if we only have 1 dataset
-      proj_list_score[[i]] = list()
-      for(j in 1 : length(proj_group[[i]])){ # j goes from 1 to 3 since we have 3 groups
-        if(proj_group[[i]][j]){
-          proj_list_score[[i]][[j]] = H[ifelse(j == 1, 1, cumsum(comp_num)[j - 1] + 1) : cumsum(comp_num)[j], ifelse(i == 1, 1, cumsum(N_dataset)[i - 1] + 1) : cumsum(N_dataset)[i]]
-        }
+
+    proj_list_score[[1]] = list()
+    for(j in 1 : length(proj_group[[1]])){ # j goes from 1 to 3 since we have 3 groups
+      if(proj_group[[1]][j]){
+        proj_list_score[[1]][[j]] = H[ifelse(j == 1, 1, cumsum(comp_num)[j - 1] + 1) : cumsum(comp_num)[j],  1:N_dataset]
       }
-    } 
+    }
     
     proj_list_score = scoreNameAssign(proj_list_score, proj_dataset_name, group_name)
     proj_list_score = sampleNameAssign(proj_list_score, proj_sample_name)
     for(i in 1 : length(proj_group[[1]])){
-      for(j in 1 : N){
-        proj_list_score[[j]][[i]] = proj_list_score[[j]][[i]] *  sqrt(ncol(proj_dataset[[j]]))
-      }
+      proj_list_score[[1]][[i]] = proj_list_score[[1]][[i]] *  sqrt(ncol(proj_dataset[[1]]))
     }
+    return(list(proj_score_list = proj_list_score))
+  } else{
+    stop("proj_dataset cannot be NULL")
   }
-  
-  return(list(proj_score_list = proj_list_score))
 }
+
+
+
+
+
+
